153a78851d690b742a57368b19d5c19f1152cb78
oversparkling <nicholasongck@gmail.com>
2021-03-25 23:03:43 +0800

    loaded files

---
+++ Pet project/Pet_projectApp.swift
@@ -0,0 +1,18 @@
+//
+//  Pet_projectApp.swift
+//  Pet project
+//
+//  Created by Nicholas Ong on 6/1/21.
+//
+
+import SwiftUI
+import UIKit
+
+@main
+struct Pet_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
