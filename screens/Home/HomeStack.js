ea9c663088c232debb0e3bda045511e475604d2b
Chiara Lim <cclim@bu.edu>
2021-08-09 16:35:43 +0800

    Edited Main Home Page

--- screens/Home/HomeStack.js
+++ screens/Home/HomeStack.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import TaskHomeScreen from "./TaskHomeScreen";
@@ -8,7 +7,7 @@ const Home = createStackNavigator();
 const HomeStack = () => {
     return (
         <Home.Navigator screenOptions={{ headerShown: false }}>
-            <Home.Screen name="TaskHomeScreen" component={TaskHomeScreen} />
+            <Home.Screen name = "TaskHomeScreen" component = { TaskHomeScreen } />
         </Home.Navigator>
     );
 };
